c139815c7c1c122c1d6ced233514d38091e7dc89
Lionel Levaux <lionel.levaux@gmail.com>
2017-04-06 16:48:24 -0400

    Fonctionnel

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  root 'pages#home'
+  resources :cocktails, only: [:show, :index, :new, :create] do
+    resources :doses, only: [:create, :destroy]
+  end
+  resources :ingredient, only: []
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
